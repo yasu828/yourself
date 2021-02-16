bd5a16f1dd936159ff1185741552dc90ab80f8a8
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-02-16 21:20:15 +0900

    メインのview作成とauthenticate_user!の記述

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,19 @@
 class AnswersController < ApplicationController
+    before_action :authenticate_user!
+
     def index
+        @answers = Answer.all.order(id: "DESC")
+    end
+
+    def create
+        answer = Answer.create(answer_params)
+        render json:{answer: post }
+    end
+
+
+    private
+
+    def answer_params
+        params.require(:answer).permit(:answer).merge(user_id: current_user.id)
     end
 end
