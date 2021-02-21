92a9a437e9fdc3a9d7c05fb423c580301014c355
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-02-21 14:04:49 +0900

    satisfactions(満足度)のルーティング記述

--- app/controllers/satisfactions_controller.rb
+++ app/controllers/satisfactions_controller.rb
@@ -1,2 +1,4 @@
 class SatisfactionsController < ApplicationController
+    def index        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'answers#index'
   
   resources :answers
+  resources :satisfactions
 end
