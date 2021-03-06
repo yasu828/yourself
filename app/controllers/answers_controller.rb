a3538c7fc9f9aec0019b31dc9ae89051c52e8f44
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-03-07 00:31:55 +0900

    深堀りしたコメントの表示

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,6 +4,7 @@ class AnswersController < ApplicationController
     def index
         @answer = Answer.new(answer_params)
         @room = Room.find(params[:room_id])
+        @answers = @room.answers.includes(:user)
     end
 
     def new
@@ -13,11 +14,15 @@ class AnswersController < ApplicationController
     def create
         
         @room = Room.find(params[:room_id])
-        @room.answers.create(
+        if @room.answers.create(
             user_id: current_user.id,
             ans: answer_params[:answer][:ans]
           )
           redirect_to room_answers_path(@room)
+        else
+            @answers = @room.ans.includes(:user)
+            render :index
+        end
         #     render json:{post: @answer}
         #   redirect_to root_path(@room)
         # else
